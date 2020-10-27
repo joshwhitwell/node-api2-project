46635de91741a42647b7c769f6e75e05a923d61f
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-27 15:55:35 -0400

    added api/posts/:id/comments post request handler

--- data/router.js
+++ data/router.js
@@ -26,21 +26,38 @@ router.get('/api/posts/:id/comments', (req, res) => {
 //[ POST ] Request Handlers
 router.post('/api/posts', (req, res) => {
     const { title, contents } = req.body
-        if(!title || !contents) {
-            res.status(400).json({ error: 'Title and contents are required' })
-        } else {
-            database.insert({ title, contents })
-                .then(post => {
-                    res.status(201).json({ message: 'Success', post })
-                })
-                .catch(err => {
-                    res.status(500).json({ message: 'Failure creating post', stack: err.stack })
-                })
-        }
+    if (!title || !contents) {
+        res.status(400).json({ error: 'Title and contents are required' })
+    } else {
+        database.insert({ title, contents })
+            .then(post => {
+                res.status(201).json({ message: 'Success', post })
+            })
+            .catch(err => {
+                res.status(500).json({ message: 'Failure creating post', stack: err.stack })
+            })
+    }
 })
 
-router.post('/api/posts', (req, res) => {
-
+router.post('/api/posts/:id/comments', (req, res) => {
+    const { text } = req.body
+    const { id } = req.params
+    try {
+        if(!text) {
+            res.status(400).json({ message: 'Text is required' })
+        } else {
+            database.insertComment({ text, post_id: id })
+                .then(comment => {
+                    res.status(201).json({ message: 'Success', comment })
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.status(404).json({ message: `No post found with ID: ${id} `})
+                })
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Failure creating comment', stack: error.stack })
+    }
 })
 
 //[ PUT ] Request Handlers
