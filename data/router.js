067290d5533176d77768f2ab0929930ea27d5397
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-27 15:13:01 -0400

    built /api/post requesst handler

--- data/router.js
+++ data/router.js
@@ -4,17 +4,53 @@ const router = express.Router()
 
 const database = require('./db.js')
 
+//[ GET ] Request Handlers
 router.get('/api/posts', (req, res) => {
     database.find()
-        .then(response => {
-            console.log(response)
-            res.end()
+        .then(posts => {
+            res.status(200).json({ message: 'Success', data: posts })
         })
         .catch(err => {
-            console.log(err.message, err.stack)
+            res.status(500).json({ message: 'Failure getting posts', stack: err.stack })
         })
 })
 
+router.get('/api/posts/:id', (req, res) => {
 
+})
+
+router.get('/api/posts/:id/comments', (req, res) => {
+
+})
+
+//[ POST ] Request Handlers
+router.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body
+        if(!title || !contents) {
+            res.status(400).json({ error: 'Title and contents are required' })
+        } else {
+            database.insert({ title, contents })
+                .then(post => {
+                    res.status(201).json({ message: 'Success', post })
+                })
+                .catch(err => {
+                    res.status(500).json({ message: 'Failure creating post', stack: err.stack })
+                })
+        }
+})
+
+router.post('/api/posts', (req, res) => {
+
+})
+
+//[ PUT ] Request Handlers
+router.put('/api/posts/:id', (req, res) => {
+
+})
+
+//[ DELETE ] Request Handlers
+router.delete('/api/posts/:id', (req, res) => {
+
+})
 
 module.exports = router
