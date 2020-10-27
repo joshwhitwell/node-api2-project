3b4f34f5f7a0eaca98ba60de89a83f5bf21438d1
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-27 17:15:19 -0400

    added /api/posts/:id delete request handler

--- data/router.js
+++ data/router.js
@@ -109,7 +109,18 @@ router.put('/api/posts/:id', (req, res) => {
 
 //[ DELETE ] Request Handlers
 router.delete('/api/posts/:id', (req, res) => {
-    
+    const { id } = req.params
+    database.remove(id)
+        .then(post => {
+            if (post === 1) {
+                res.status(200).json({ message: 'Success', itemsDeleted: post })
+            } else {
+                res.status(404).json({ message: `Post not found with ID: ${id}` })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failure deleting post', stack: err.stack })
+        })
 })
 
 module.exports = router
